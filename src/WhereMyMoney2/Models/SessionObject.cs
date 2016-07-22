add921a8663c5a560efb554e36ce15ba13ce6ddf
kevin <kevindu1986@outlook.com>
2016-07-22 17:30:11 +0700

    Build RC 2.01: - Connect app 1.0 database

---
+++ src/WhereMyMoney2/Models/SessionObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereMyMoney.Models
+{
+    public class SessionObject
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ src/WhereMyMoney2/Models/Tbl_TransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereMyMoney.Models
+{
+    public class Tbl_TransactionType
+    {
+        public const string Income = "Income";
+        public const string Outcome = "Outcome";
+        public const string Transfer = "Transfer";
+    }
+}
